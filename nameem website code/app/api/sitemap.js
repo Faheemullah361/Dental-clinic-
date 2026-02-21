f0c5f81c1ab60d737f58d1e4a1c0aec758d41e05
Faheem Ullah <154213795+Faheemullah361@users.noreply.github.com>
2026-02-21 13:29:42 +0500

    Switch to plain JavaScript sitemap API for reliable Vercel deployment

--- nameem website code/app/api/sitemap.js
+++ nameem website code/app/api/sitemap.js
@@ -1,6 +1,4 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
+module.exports = (req, res) => {
   const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
@@ -14,4 +12,4 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
   res.setHeader('Content-Type', 'application/xml; charset=utf-8');
   res.setHeader('Cache-Control', 'public, s-maxage=3600, stale-while-revalidate=86400');
   res.status(200).send(sitemapXml);
-}
+};
