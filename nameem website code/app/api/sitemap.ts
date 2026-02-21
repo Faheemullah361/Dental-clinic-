a8fd2c96b8c147ab7693ecfd5b313a449c424904
Faheem Ullah <154213795+Faheemullah361@users.noreply.github.com>
2026-02-21 13:22:12 +0500

    Create serverless sitemap API endpoint for reliable Vercel serving

---
+++ nameem website code/app/api/sitemap.ts
@@ -0,0 +1,17 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://dental-clinic-q3pp.vercel.app/</loc>
+    <lastmod>2026-02-21</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+</urlset>`;
+
+  res.setHeader('Content-Type', 'application/xml; charset=utf-8');
+  res.setHeader('Cache-Control', 'public, s-maxage=3600, stale-while-revalidate=86400');
+  res.status(200).send(sitemapXml);
+}
